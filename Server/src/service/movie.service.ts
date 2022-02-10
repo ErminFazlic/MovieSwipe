c972d9c685678e8bf740d952362ba853c4846600
ErminFazlic <Erminfazlic97@gmail.com>
2022-02-10 14:46:05 +0100

    Updated movie service to use in memory store

--- Server/src/service/movie.service.ts
+++ Server/src/service/movie.service.ts
@@ -1,10 +1,18 @@
 import {Movie} from "../model/movie.interface"
 
-const liked : number[] = [];
 
-export const addLike = async (id: number, userID: number) : Promise<Movie> => {
-    liked.push(userID);
-    const movie : Movie = {id : new Date().valueOf(), likes : liked};
-    return movie;
+// In-Memory Store
+const movies : { [key: number]: Movie } = {};
+
+export const addLike = async (id: number, userID: number) : Promise<Array<number>> => {
+    if(!(id in movies)){
+        const newID = new Date().valueOf();
+        movies[newID] = { id : newID, likes : []};
+        id = newID;
+        }
+    const movie :Movie = movies[id];
+    movie.likes.push(userID);
+    movies[id] = movie;
+    return movie.likes;
 }
 
