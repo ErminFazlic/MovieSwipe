4826dc5a11a3625bc103c4e1d8cd31f68dac8b89
arian <arian.ka.1990@gmail.com>
2022-02-23 19:53:54 +0100

    Initial commit for lab 3

---
+++ Client/v1/src/Components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const Login = () => {
+  return (
+    <div><p>I'm Login</p></div>
+  )
+}
+
+export default Login
---
+++ Client/v1/src/Components/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const Register = () => {
+  return (
+    <div><p>I'm register</p></div>
+  )
+}
+
+export default Register
